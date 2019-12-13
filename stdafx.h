b3a39d4d09dda06287f74ddfdcfb9821e9ade204
UsingNamespaceNolan <52221753+UsingNamespaceNolan@users.noreply.github.com>
2019-12-13 10:01:44 -0500

    Add files via upload

---
+++ stdafx.h
@@ -0,0 +1,16 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+#pragma once
+#include <conio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <msclr\marshal_cppstd.h>
+#include <fstream>
+#include <cmath>
+#include <iomanip>
+#include <cstdlib>
+#include <iterator>
+#include <algorithm>
+// TODO: reference additional headers your program requires here
